70bafcd435a6cbd1a4a61f522082a4cc4c14aad1
bessybabu <bessybabu88@gmail.com>
2019-04-24 20:23:41 +0530

    modified

--- AVG.sh
+++ AVG.sh
@@ -1,5 +1,11 @@
 echo enter 5 numbers
 read a b c d e
-sum=`expr $a+$b+$c+$d+$e`
-echo $sum
+s=` expr $a + $b + $c + $d + $e `
+avg=` expr $s / 5 `
+echo  average is $avg
+fact=1
+for((i=1;i<=avd;i++))
+do
+fact=`expr $fact \* $i `
+echo $fact
 
